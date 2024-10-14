0bd5fad25671b062941c46c83d9682117470905f
IvanMendoza1616 <navi96@hotmail.es>
2024-10-14 13:30:56 -0600

    added search icon

--- app/components/navbar/SearchInput.tsx
+++ app/components/navbar/SearchInput.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useQueryParams } from "@/app/hooks/useQueryParams";
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import { FormEvent, useEffect, useRef, useState } from "react";
 
 export default function SearchInput() {
@@ -20,9 +21,10 @@ export default function SearchInput() {
 
   return (
     <form
-      className="col-span-3 flex items-center justify-center gap-2 md:col-span-1"
+      className="col-span-3 flex items-center justify-center gap-0 overflow-hidden rounded-md border focus-within:outline focus-within:outline-primary md:col-span-1"
       onSubmit={handleSubmit}
     >
+      <MagnifyingGlassIcon className="ml-4 mr-2 h-5 w-5 text-gray-400" />
       <input
         placeholder="Search pets..."
         ref={inputRef}
@@ -33,7 +35,7 @@ export default function SearchInput() {
         type="text"
         name="search"
         id="search"
-        className="w-full rounded-md border px-3 py-2 focus:outline-primary"
+        className="w-full py-2 pr-3 focus:outline-none"
       />
     </form>
   );
